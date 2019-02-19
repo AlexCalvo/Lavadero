99e9b664738d3cbfea38dc094d6d5ea55f198bdf
jfaldanam <jfaldanam@gmail.com>
2019-02-19 10:16:28 +0100

    Added icon and title to the application

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 import DB.DatabaseException;
 import DB.MySqlDB;
+import UI.GUILaunch;
 import UI.GUIMain;
 
 import javax.swing.*;
@@ -7,23 +8,7 @@ import java.awt.*;
 
 public class Test {
 
-    private final static String configFile = "database.config";
-
     public static void main(String[] args) {
-        JFrame frame = new JFrame("");
-        JTabbedPane panel = new GUIMain();
-
-        frame.setContentPane(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setMinimumSize(new Dimension(640, 480));
-        frame.pack();
-        frame.setVisible(true);
-
-
-        try (MySqlDB db = new MySqlDB()) {
-
-        } catch (DatabaseException e) {
-            System.err.println(e);
-        }
+        JFrame frame = GUILaunch.createGUI();
     }
 }
--- src/UI/GUILaunch.java
+++ src/UI/GUILaunch.java
@@ -1,9 +1,20 @@
 package UI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GUILaunch {
-    public static void createGUI(JFrame window) {
+    public static JFrame createGUI() {
+        JFrame window = new JFrame("LavaderOS");
+        JTabbedPane panel = new GUIMain();
 
+        window.setIconImage(new ImageIcon("logo.png").getImage());
+        window.setContentPane(panel);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setMinimumSize(new Dimension(640, 480));
+        window.pack();
+        window.setVisible(true);
+
+        return window;
     }
 }
