abfecc60644fb7d1804c8bc5a4600216b3c25786
AlexCalvo <alexccmadrid@uma.es>
2019-04-06 17:40:48 +0200

    changes on complementos

--- src/Controllers/CtrComplementos.java
+++ src/Controllers/CtrComplementos.java
@@ -11,71 +11,68 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class CtrComplementos implements ActionListener, ListSelectionListener {
-    private GUIComplementos view;
-    private Complementos selectedComplemento;
+	private GUIComplementos view;
+	private Complementos selectedComplemento;
 
-    public CtrComplementos(GUIComplementos view) {
-        this.view = view;
-    }
+	public CtrComplementos(GUIComplementos view) {
+		this.view = view;
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
-        switch (actionEvent.getActionCommand()) {
-            case "Insertar":
-                if (!view.getFieldId().equals("")) {
-                    selectedComplemento = new Complementos(view.getFieldNombre(),view.getFieldPrecio());
-                    view.reloadData();
-                    view.setFieldId("");
-                    view.setFieldNombre("");
-                    view.setFieldPrecio("");
-                    selectedComplemento = null;
-                }
-                break;
-            case "Modificar":
-                if (selectedComplemento != null) {
-                    if (!view.getFieldNombre().equals("")) {
-                    	selectedComplemento.setNombre(view.getFieldNombre());
-                    	selectedComplemento.setPrecio(view.getFieldPrecio());
-                        view.reloadData();
-                        view.setFieldId("");
-                        view.setFieldNombre("");
-                        view.setFieldPrecio("");
-                        selectedComplemento = null;
-                    }
-                }
-                break;
-            case "Eliminar":
-                if (selectedComplemento != null) {
-                    selectedComplemento.delete();
-                    view.reloadData();
-                    view.setFieldId("");
-                    view.setFieldNombre("");
-                    view.setFieldPrecio("");
-                    selectedComplemento = null;
-                }
-                break;
-            default:
-                break;
-        }
-    }
+	@Override
+	public void actionPerformed(ActionEvent actionEvent) {
+		switch (actionEvent.getActionCommand()) {
+		case "Insertar":
+			if (!view.getFieldNombre().equals("")) {
+				selectedComplemento = new Complementos(view.getFieldNombre(), view.getFieldPrecio());
+				view.reloadData();
+				view.setFieldNombre("");
+				view.setFieldPrecio("");
+				selectedComplemento = null;
+			}
+			break;
+		case "Modificar":
+			if (selectedComplemento != null) {
+				if (!view.getFieldNombre().equals("")) {
+					selectedComplemento.setNombre(view.getFieldNombre());
+					selectedComplemento.setPrecio(view.getFieldPrecio());
+					view.reloadData();
+					view.setFieldNombre("");
+					view.setFieldPrecio("");
+					selectedComplemento = null;
+				}
+			}
+			break;
+		case "Eliminar":
+			if (selectedComplemento != null) {
+				selectedComplemento.delete();
+				view.reloadData();
+				view.setFieldNombre("");
+				view.setFieldPrecio("");
+				selectedComplemento = null;
+			}
+			break;
+		default:
+			break;
+		}
+	}
 
-    @Override
-    public void valueChanged(ListSelectionEvent listSelectionEvent) {
-        if (!listSelectionEvent.getValueIsAdjusting()) {
+	@Override
+	public void valueChanged(ListSelectionEvent listSelectionEvent) {
+		if (!listSelectionEvent.getValueIsAdjusting()) {
 
-            int firstindex = listSelectionEvent.getFirstIndex();
-            int lastIndex = listSelectionEvent.getLastIndex();
-            ListSelectionModel selectionModel = view.getTable().getSelectionModel();
-            TableModel model = view.getTable().getModel();
-            if (selectionModel.isSelectionEmpty())
-                return;
-            String id = (String) ((selectionModel.isSelectedIndex(lastIndex)) ? model.getValueAt(lastIndex, 0) : model.getValueAt(firstindex, 0));
+			int firstindex = listSelectionEvent.getFirstIndex();
+			int lastIndex = listSelectionEvent.getLastIndex();
+			ListSelectionModel selectionModel = view.getTable().getSelectionModel();
+			TableModel model = view.getTable().getModel();
+			if (selectionModel.isSelectionEmpty())
+				return;
+			String id = (String) ((selectionModel.isSelectedIndex(lastIndex)) ? model.getValueAt(lastIndex, 0)
+					: model.getValueAt(firstindex, 0));
 
+			selectedComplemento = new Complementos(id);
+			view.setFieldNombre(selectedComplemento.getNombre());
+			view.setFieldPrecio(selectedComplemento.getPrecio() + "");
 
-            selectedComplemento = new Complementos(id);
-            view.setFieldNombre(selectedComplemento.getNombre());
-            view.setFieldPrecio(selectedComplemento.getPrecio()+"");
-
-        }
-    }
+		}
+	}
 }
--- src/UI/GUIComplementos.java
+++ src/UI/GUIComplementos.java
@@ -11,156 +11,139 @@ import java.awt.event.ActionListener;
 import java.util.List;
 
 public class GUIComplementos extends GUIPanel {
-    private JTabbedPane tabPanel;
-    private Component panelGeneral;
-    private JTable tableComplemento;
+	private JTabbedPane tabPanel;
+	private Component panelGeneral;
+	private JTable tableComplemento;
 
-    private JLabel lNombre = new JLabel("Nombre:");
-    private JLabel lID = new JLabel("ID:");
-    private JLabel lPrecio = new JLabel("Precio:");
+	private JLabel lNombre = new JLabel("Nombre:");
+	private JLabel lPrecio = new JLabel("Precio:");
+	
+	private JTextField tNombre = new JTextField();
+	private JTextField tPrecio = new JTextField();
 
-    private JTextField tID = new JTextField();
-    private JTextField tNombre = new JTextField();
-    private JTextField tPrecio = new JTextField();
+	private JButton bIns = new JButton("Insertar");
+	private JButton bMod = new JButton("Modificar");
+	private JButton bEli = new JButton("Eliminar");
 
-    private JButton bIns = new JButton("Insertar");
-    private JButton bMod = new JButton("Modificar");
-    private JButton bEli = new JButton("Eliminar");
+	public GUIComplementos() {
+		createPanelComplementos();
+	}
 
-    public GUIComplementos() {
-        createPanelComplementos();
-    }
+	private void createPanelComplementos() {
+		this.setLayout(new BorderLayout());
 
-    private void createPanelComplementos() {
-        this.setLayout(new BorderLayout());
+		tableComplemento = new JTable(new ComplementoTableModel());
+		tableComplemento.setRowHeight(25);
+		tableComplemento.setFont(new java.awt.Font("Tahoma", 0, 15));
+		this.add(new JScrollPane(tableComplemento), BorderLayout.CENTER);
 
-        tableComplemento = new JTable(new ComplementoTableModel());
-        tableComplemento.setRowHeight(25);
-        tableComplemento.setFont(new java.awt.Font("Tahoma", 0, 15)); 
-        this.add(new JScrollPane(tableComplemento), BorderLayout.CENTER);
+		this.add(createSouthPanel(), BorderLayout.SOUTH);
+	}
 
-        this.add(createSouthPanel(), BorderLayout.SOUTH);
-    }
+	private Component createSouthPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(2, 2, 5, 5));
 
-    private Component createSouthPanel() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(2, 2, 5, 5));
+		panel.add(create2ElementPanel(lNombre, tNombre));
+		panel.add(create2ElementPanel(lPrecio, tPrecio));
+		panel.add(createButtonPanel());
 
-        panel.add(create2ElementPanel(lID, tID));
-        panel.add(create2ElementPanel(lNombre, tNombre));
-        panel.add(create2ElementPanel(lPrecio, tPrecio));
-        panel.add(createButtonPanel());
+		return panel;
+	}
 
+	public String getFieldNombre() {
+		return this.tNombre.getText();
+	}
 
-        return panel;
-    }
-    
-    
-    public String getFieldNombre() {
-        return this.tNombre.getText();
-    }
+	public void setFieldNombre(String string) {
+		this.tNombre.setText(string);
+	}
 
-    public void setFieldNombre(String string) {
-        this.tNombre.setText(string);
-    }
-    
-    public String getFieldId() {
-        return this.tID.getText();
-    }
+	public double getFieldPrecio() {
+		return Double.parseDouble(this.tPrecio.getText());
+	}
 
-    public void setFieldId(String string) {
-        this.tID.setText(string);
-    }
-   
+	public void setFieldPrecio(String d) {
+		this.tPrecio.setText(d);
+	}
 
-    public double getFieldPrecio() {
-        return Double.parseDouble(this.tPrecio.getText());
-    }
-    
-    public void setFieldPrecio(String d) {
-        this.tPrecio.setText(d);
-     }
-    
+	private Component createButtonPanel() {
+		JPanel panelBotones = new JPanel();
+		panelBotones.setLayout(new FlowLayout());
+		panelBotones.add(bIns);
+		panelBotones.add(bMod);
+		panelBotones.add(bEli);
+		return panelBotones;
+	}
 
-    private Component createButtonPanel() {
-        JPanel panelBotones = new JPanel();
-        panelBotones.setLayout(new FlowLayout());
-        panelBotones.add(bIns);
-        panelBotones.add(bMod);
-        panelBotones.add(bEli);
-        return panelBotones;
-    }
+	public void reloadData() {
+		this.tableComplemento.getSelectionModel().clearSelection();
+		((ComplementoTableModel) tableComplemento.getModel()).reloadData();
+	}
 
-    public void reloadData() {
-        this.tableComplemento.getSelectionModel().clearSelection();
-        ((ComplementoTableModel) tableComplemento.getModel()).reloadData();
-    }
+	public JTable getTable() {
+		return tableComplemento;
+	}
 
-    public JTable getTable() {
-        return tableComplemento;
-    }
+	@Override
+	public void addController(ActionListener ctr) {
+		bIns.addActionListener(ctr);
+		bIns.setActionCommand("Insertar");
+		bMod.addActionListener(ctr);
+		bMod.setActionCommand("Modificar");
+		bEli.addActionListener(ctr);
+		bEli.setActionCommand("Eliminar");
+		tableComplemento.getSelectionModel().addListSelectionListener((ListSelectionListener) ctr);
+	}
 
-    @Override
-    public void addController(ActionListener ctr) {
-        bIns.addActionListener(ctr);
-        bIns.setActionCommand("Insertar");
-        bMod.addActionListener(ctr);
-        bMod.setActionCommand("Modificar");
-        bEli.addActionListener(ctr);
-        bEli.setActionCommand("Eliminar");
-        tableComplemento.getSelectionModel().addListSelectionListener((ListSelectionListener) ctr);
-    }
-    
-    
-    private class ComplementoTableModel extends AbstractTableModel {
+	private class ComplementoTableModel extends AbstractTableModel {
 
-        private String[] columnNames;
-        private Object[][] data;
+		private String[] columnNames;
+		private Object[][] data;
 
-        public ComplementoTableModel() {
-            columnNames = Complementos.columnas;
-            List<Complementos> lista = Complementos.listaComplementos();
-            data = new Object[lista.size()][columnNames.length];
-            for (int i = 0; i < lista.size(); i++) {
-                data[i] = lista.get(i).asArray();
-            }
-        }
-        
-        public void reloadData() {
-            List<Complementos> lista = Complementos.listaComplementos();
-            data = new Object[lista.size()][columnNames.length];
-            for (int i = 0; i < lista.size(); i++) {
-                data[i] = lista.get(i).asArray();
-            }
-            fireTableDataChanged();
-        }
+		public ComplementoTableModel() {
+			columnNames = Complementos.columnas;
+			List<Complementos> lista = Complementos.listaComplementos();
+			data = new Object[lista.size()][columnNames.length];
+			for (int i = 0; i < lista.size(); i++) {
+				data[i] = lista.get(i).asArray();
+			}
+		}
 
+		public void reloadData() {
+			List<Complementos> lista = Complementos.listaComplementos();
+			data = new Object[lista.size()][columnNames.length];
+			for (int i = 0; i < lista.size(); i++) {
+				data[i] = lista.get(i).asArray();
+			}
+			fireTableDataChanged();
+		}
 
-        @Override
-        public int getRowCount() {
-            return data.length;
-        }
+		@Override
+		public int getRowCount() {
+			return data.length;
+		}
 
-        @Override
-        public String getColumnName(int col) {
-            return columnNames[col];
-        }
+		@Override
+		public String getColumnName(int col) {
+			return columnNames[col];
+		}
 
-        @Override
-        public int getColumnCount() {
-            return columnNames.length;
-        }
+		@Override
+		public int getColumnCount() {
+			return columnNames.length;
+		}
 
-        @Override
-        public Object getValueAt(int row, int col) {
-            return data[row][col];
-        }
+		@Override
+		public Object getValueAt(int row, int col) {
+			return data[row][col];
+		}
 
-        @Override
-        public void setValueAt(Object value, int row, int col) {
-            data[row][col] = value;
-            fireTableCellUpdated(row, col);
-        }
-    }
+		@Override
+		public void setValueAt(Object value, int row, int col) {
+			data[row][col] = value;
+			fireTableCellUpdated(row, col);
+		}
+	}
 
 }
